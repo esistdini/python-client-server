78cb1d41e4a2eeb4e3469de210a4a6535be79c15
it's harry <148378673+25-WHITEHACKER@users.noreply.github.com>
2024-07-03 20:18:15 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,2 +1,16 @@
 import socket
 
+def start_client():
+    host = '127.0.0.1' 
+    port = 4444
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        while True:
+            message = input("Client: ")
+            s.sendall(message.encode())
+            data = s.recv(1024)
+            print('Received from server:', data.decode())
+
+
+start_client()
