79bdd62c0f1b58ebb02eb10044399709532d0b0f
it's harry <148378673+25-WHITEHACKER@users.noreply.github.com>
2024-07-03 20:16:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
 import socket
+
+def start_server():
+    host = '127.0.0.1'
+    port = 4444       
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+start_server()
